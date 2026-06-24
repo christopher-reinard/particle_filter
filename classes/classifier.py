09700820ab3e36f052ce60af7a6b7455e48c97e6
PeterMoehle <116306085+PeterMoehle@users.noreply.github.com>
2026-06-24 23:14:34 +0200

    Use log normalization factor in evaluate_log_likelihood
    Refactor evaluate_log_likelihood to use log normalization factor.

--- classes/classifier.py
+++ classes/classifier.py
@@ -27,6 +27,8 @@ class GaussianClassifier(StatisticalClassifier):
         det_cov = np.linalg.det(measurement_cov)
         self.normalization_factor = 1.0 / np.sqrt(((2.0 * np.pi) ** self.dim) * det_cov)
 
+        self.log_norm_factor = np.log(self.normalization_factor)
+
     def evaluate(self, observation: np.ndarray, predicted_measurement: np.ndarray) -> float:
         residual = observation - predicted_measurement
         exponent = -0.5 * (residual.T @ self.inv_cov @ residual)
@@ -35,5 +37,5 @@ class GaussianClassifier(StatisticalClassifier):
     def evaluate_log_likelihood(self, observation: np.ndarray, predicted_measurement: np.ndarray) -> float:
         residual = observation - predicted_measurement
         # Use log-likelihood: log(p) = -0.5 * (res^T @ inv_cov @ res) - 0.5 * log(det(cov)) - const
-        # So we can sum it up easily. And constant doesnt matter
-        return -0.5 * (residual.T @ self.inv_cov @ residual)
+        # So we can sum it up easily. And constant doesnt matter, we integrate it still
+        return self.log_norm_factor + (-0.5 * (residual.T @ self.inv_cov @ residual))
